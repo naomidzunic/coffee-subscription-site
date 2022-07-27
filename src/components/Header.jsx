5d9050dcbc2a9bde0ccc8d9a852476977db6e1e7
naomidzunic <wildandfreeservices@gmail.com>
2022-07-26 22:29:23 -0500

    tweaking device responsivness for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
 import CSS from './Header.css';
+import frenchPress from '../images/mobile/image-hero-coffeepress.jpg';
+
 
 
 function Header() {
   return (
     <div>
-      
+      <div className="header-container">
+        <div className="header-content">
+          <div className="header-txt">
+          <h1>Great coffee made simple.</h1>
+          <p>
+            Start your mornings with the world’s best coffees. Try our expertly
+            curated artisan coffees from our best roasters delivered directly to
+            your door, at your schedule.
+          </p>
+          <button>Create your plan</button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Header
+
+
+// OVERLAPPING IMAGE AND TEXT
